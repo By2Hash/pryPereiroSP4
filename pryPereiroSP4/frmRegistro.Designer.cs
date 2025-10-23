a97bf8e268b14e8194f8378b95a951fc214f92c6
By2Hash <by2git2@gmail.com>
2025-10-23 20:10:53 -0300

    Falta hacer lo de validar cual es el mozo con mayor venta

--- pryPereiroSP4/frmRegistro.Designer.cs
+++ pryPereiroSP4/frmRegistro.Designer.cs
@@ -36,6 +36,11 @@
             colBebidasConAlcohol = new DataGridViewTextBoxColumn();
             colPostres = new DataGridViewTextBoxColumn();
             btnValidar = new Button();
+            btnLimpiar = new Button();
+            lblConsulta = new Label();
+            button1 = new Button();
+            lblMozoDelDia = new Button();
+            lblRespuestaMozos = new Label();
             ((System.ComponentModel.ISupportInitialize)dgvResistro).BeginInit();
             SuspendLayout();
             // 
@@ -45,62 +50,130 @@
             dgvResistro.AllowUserToAddRows = false;
             dgvResistro.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dgvResistro.Columns.AddRange(new DataGridViewColumn[] { colMozos, colComidas, colBebidasSinAlcohol, colBebidasConAlcohol, colPostres });
-            dgvResistro.Location = new Point(12, 12);
+            dgvResistro.Location = new Point(17, 20);
+            dgvResistro.Margin = new Padding(4, 5, 4, 5);
             dgvResistro.Name = "dgvResistro";
             dgvResistro.RowHeadersVisible = false;
-            dgvResistro.Size = new Size(504, 269);
+            dgvResistro.RowHeadersWidth = 62;
+            dgvResistro.Size = new Size(720, 350);
             dgvResistro.TabIndex = 0;
             dgvResistro.CellContentClick += dgvResistro_CellContentClick;
             // 
             // colMozos
             // 
             colMozos.HeaderText = "Mozos";
+            colMozos.MinimumWidth = 8;
             colMozos.Name = "colMozos";
+            colMozos.Width = 150;
             // 
             // colComidas
             // 
             colComidas.HeaderText = "comidas";
+            colComidas.MinimumWidth = 8;
             colComidas.Name = "colComidas";
+            colComidas.Width = 150;
             // 
             // colBebidasSinAlcohol
             // 
             colBebidasSinAlcohol.HeaderText = "Bebidas sin alchol";
+            colBebidasSinAlcohol.MinimumWidth = 8;
             colBebidasSinAlcohol.Name = "colBebidasSinAlcohol";
+            colBebidasSinAlcohol.Width = 150;
             // 
             // colBebidasConAlcohol
             // 
             colBebidasConAlcohol.HeaderText = "Bebidas con alcohol";
+            colBebidasConAlcohol.MinimumWidth = 8;
             colBebidasConAlcohol.Name = "colBebidasConAlcohol";
+            colBebidasConAlcohol.Width = 150;
             // 
             // colPostres
             // 
             colPostres.HeaderText = "Postres";
+            colPostres.MinimumWidth = 8;
             colPostres.Name = "colPostres";
+            colPostres.Width = 150;
             // 
             // btnValidar
             // 
-            btnValidar.Location = new Point(393, 287);
+            btnValidar.Location = new Point(594, 380);
+            btnValidar.Margin = new Padding(4, 5, 4, 5);
             btnValidar.Name = "btnValidar";
-            btnValidar.Size = new Size(123, 48);
+            btnValidar.Size = new Size(143, 45);
             btnValidar.TabIndex = 1;
-            btnValidar.Text = "Validar";
+            btnValidar.Text = "Validar Datos";
             btnValidar.UseVisualStyleBackColor = true;
             btnValidar.Click += btnValidar_Click;
             // 
+            // btnLimpiar
+            // 
+            btnLimpiar.Location = new Point(452, 380);
+            btnLimpiar.Name = "btnLimpiar";
+            btnLimpiar.Size = new Size(126, 45);
+            btnLimpiar.TabIndex = 2;
+            btnLimpiar.Text = "Limpiar";
+            btnLimpiar.UseVisualStyleBackColor = true;
+            btnLimpiar.Click += btnLimpiar_Click;
+            // 
+            // lblConsulta
+            // 
+            lblConsulta.AutoSize = true;
+            lblConsulta.Location = new Point(10, 433);
+            lblConsulta.Name = "lblConsulta";
+            lblConsulta.Size = new Size(734, 25);
+            lblConsulta.TabIndex = 4;
+            lblConsulta.Text = "- - - - - - - - - - - - - - - - - - - - - - - - - - - Consulta - - - - - - - - - - - - - - - - - - - - - - - - - - - ";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(594, 533);
+            button1.Name = "button1";
+            button1.Size = new Size(143, 45);
+            button1.TabIndex = 5;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // lblMozoDelDia
+            // 
+            lblMozoDelDia.Location = new Point(452, 533);
+            lblMozoDelDia.Name = "lblMozoDelDia";
+            lblMozoDelDia.Size = new Size(126, 45);
+            lblMozoDelDia.TabIndex = 6;
+            lblMozoDelDia.Text = "Mozo del día";
+            lblMozoDelDia.UseVisualStyleBackColor = true;
+            lblMozoDelDia.Click += lblMozoDelDia_Click;
+            // 
+            // lblRespuestaMozos
+            // 
+            lblRespuestaMozos.Location = new Point(17, 476);
+            lblRespuestaMozos.Name = "lblRespuestaMozos";
+            lblRespuestaMozos.Size = new Size(720, 54);
+            lblRespuestaMozos.TabIndex = 7;
+            // 
             // frmRegistro
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AcceptButton = btnValidar;
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(530, 392);
+            BackColor = Color.White;
+            ClientSize = new Size(756, 616);
+            Controls.Add(lblRespuestaMozos);
+            Controls.Add(lblMozoDelDia);
+            Controls.Add(button1);
+            Controls.Add(lblConsulta);
+            Controls.Add(btnLimpiar);
             Controls.Add(btnValidar);
             Controls.Add(dgvResistro);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
             Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(4, 5, 4, 5);
             Name = "frmRegistro";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "La Turca - Registro de venta";
             Load += frmRegistro_Load;
             ((System.ComponentModel.ISupportInitialize)dgvResistro).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -112,5 +185,10 @@
         private DataGridViewTextBoxColumn colBebidasConAlcohol;
         private DataGridViewTextBoxColumn colPostres;
         private Button btnValidar;
+        private Button btnLimpiar;
+        private Label lblConsulta;
+        private Button button1;
+        private Button lblMozoDelDia;
+        private Label lblRespuestaMozos;
     }
 }
--- pryPereiroSP4/frmRegistro.cs
+++ pryPereiroSP4/frmRegistro.cs
@@ -1,3 +1,5 @@
+using System.Windows.Forms;
+
 namespace pryPereiroSP4
 {
     public partial class frmRegistro : Form
@@ -11,11 +13,18 @@ namespace pryPereiroSP4
 
         private void frmRegistro_Load(object sender, EventArgs e)
         {
+
+
+
+
+
             dgvResistro.Rows.Add("Julio");
             dgvResistro.Rows.Add("Esteban");
             dgvResistro.Rows.Add("Javier");
             dgvResistro.Rows.Add("Gonzalo");
             dgvResistro.Rows.Add("Alberto");
+
+
 
             dgvResistro.Rows[0].Cells[0].Style.BackColor = Color.LightGray;
             dgvResistro.Rows[1].Cells[0].Style.BackColor = Color.LightGray;
@@ -37,7 +46,7 @@ namespace pryPereiroSP4
         {
             for (int indiceFila = 0; indiceFila < dgvResistro.Rows.Count; indiceFila++)
             {
-                for(int indiceColumna = 1; indiceColumna < dgvResistro.Rows.Count; indiceColumna++)
+                for (int indiceColumna = 1; indiceColumna < dgvResistro.Rows.Count; indiceColumna++)
                 {
                     var valorCelda = dgvResistro.Rows[indiceFila].Cells[indiceColumna].Value;
 
@@ -49,17 +58,67 @@ namespace pryPereiroSP4
                     {
                         if (float.TryParse(valorCelda.ToString(), out float contenidoCelda))
                         {
-                           
+
                             dgvResistro.Rows[indiceFila].Cells[indiceColumna].Value = "si";
                         }
                         else
                         {
                             dgvResistro.Rows[indiceFila].Cells[indiceColumna].Value = "no";
+
                         }
 
                     }
+
+                }
+            }
+            MessageBox.Show("Algunos datos ingresados, son incorrectos");
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            for (int indiceFila = 0; indiceFila < dgvResistro.Rows.Count; indiceFila++)
+            {
+                for (int indiceColumna = 1; indiceColumna < dgvResistro.Rows.Count; indiceColumna++)
+                {
+                    var valorCelda = dgvResistro.Rows[indiceFila].Cells[indiceColumna].Value;
+
+                    if (valorCelda == null || string.IsNullOrWhiteSpace(valorCelda.ToString()))
+                    {
+                        dgvResistro.Rows[indiceFila].Cells[indiceColumna].Value = "";
+                    }
+                    else
+                    {
+                        if (float.TryParse(valorCelda.ToString(), out float contenidoCelda))
+                        {
+
+                            dgvResistro.Rows[indiceFila].Cells[indiceColumna].Value = "";
+                        }
+                        else
+                        {
+                            dgvResistro.Rows[indiceFila].Cells[indiceColumna].Value = "";
+
+                        }
+
+                    }
+
                 }
             }
         }
+
+        private void lblMozoDelDia_Click(object sender, EventArgs e)
+        {
+            int majorventa = 0;
+            string mozo;
+
+            for(int iFila = 0; iFila < dgvResistro.Rows.Count; iFila++)
+            {
+                for(int iColumna = 0; iColumna < dgvResistro.Rows.Count; iColumna++)
+                {
+                    majorventa = dgvResistro.Rows.Count;
+                }
+            }
+            
+        }
     }
+
 }
